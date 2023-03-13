fbc3a645004137925b0a7269bd1c636804c95bf4
Abc Def Mac (SourceTree) <69718370+javasamplecodes@users.noreply.github.com>
2023-03-13 11:11:35 +0300

    Update SpringSecurityConfiguration.java

--- 11-web-application/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ 11-web-application/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -4,7 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.function.Function;
 
 @Configuration
 public class SpringSecurityConfiguration {
@@ -13,13 +17,31 @@ public class SpringSecurityConfiguration {
 
     @Bean
     public InMemoryUserDetailsManager createUserDetailsManager() {
-        UserDetails userDetails = User.withDefaultPasswordEncoder()
+        Function<String, String> passwordEncoder =
+                input -> passwordEncoder().encode(input);
+
+        // pwd is encoded here
+        UserDetails userDetails = User.builder()
+                .passwordEncoder(passwordEncoder)
                 .username("omer")
                 .password("123")
                 .roles("USER", "ADMIN")
                 .build();
 
+        // withDefaultPasswordEncoder is deprecated
+        /*UserDetails userDetails = User.withDefaultPasswordEncoder()
+                .username("omer")
+                .password("123")
+                .roles("USER", "ADMIN")
+                .build();*/
+
         return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    // used as decoder
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
