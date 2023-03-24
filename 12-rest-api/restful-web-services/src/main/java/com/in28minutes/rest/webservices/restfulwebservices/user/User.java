e823c5a7417e4e73d2604bf591d7098673384bd0
Abc Def Mac (SourceTree) <69718370+javasamplecodes@users.noreply.github.com>
2023-03-24 12:29:48 +0300

    Create User.java

---
+++ 12-rest-api/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -0,0 +1,48 @@
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+
+import java.time.LocalDate;
+
+public class User {
+    private Integer Id;
+    private String name;
+    private LocalDate birthDate;
+
+    public User(Integer id, String name, LocalDate birthDate) {
+        Id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "Id=" + Id +
+                ", name='" + name + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
+    }
+}
