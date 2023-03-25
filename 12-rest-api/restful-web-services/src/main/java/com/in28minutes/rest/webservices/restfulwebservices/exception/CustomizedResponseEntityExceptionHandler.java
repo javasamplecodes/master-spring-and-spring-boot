e2a14d4670c64e05dd3accae2f2ddaacb6ba38f5
Abc Def Mac (SourceTree) <69718370+javasamplecodes@users.noreply.github.com>
2023-03-25 17:21:34 +0300

    Update CustomizedResponseEntityExceptionHandler.java

--- 12-rest-api/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
+++ 12-rest-api/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,14 +1,19 @@
 package com.in28minutes.rest.webservices.restfulwebservices.exception;
 
 import com.in28minutes.rest.webservices.restfulwebservices.user.UserNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
@@ -27,6 +32,23 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
                 ex.getMessage(), request.getDescription(false));
 
         return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
+                "Total Errors:" + ex.getErrorCount() + ", Errors: " + constructErrorMessagesFromBindException(ex),
+                request.getDescription(false));
+
+        return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+    private String constructErrorMessagesFromBindException(BindException ex) {
+        StringBuilder errorMessageBuilder = new StringBuilder();
+        ex.getFieldErrors().stream()
+                .forEach(fieldError -> errorMessageBuilder.append(fieldError.getDefaultMessage()).append(" | "));
+        return errorMessageBuilder.toString().trim();
     }
 
 }
