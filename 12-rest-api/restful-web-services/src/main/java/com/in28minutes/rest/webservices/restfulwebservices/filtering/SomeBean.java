17761826ba389fc0b9501516ab3dce07f8c8959d
Abc Def Mac (SourceTree) <69718370+javasamplecodes@users.noreply.github.com>
2023-03-28 11:25:15 +0300

    Create SomeBean.java

---
+++ 12-rest-api/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/filtering/SomeBean.java
@@ -0,0 +1,46 @@
+package com.in28minutes.rest.webservices.restfulwebservices.filtering;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+//@JsonIgnoreProperties("field1")
+//@JsonIgnoreProperties({"field1", "field2"})
+public class SomeBean {
+    private String field1;
+
+    // prefer @JsonIgnore rather than @JsonIgnoreProperties
+    // because if field name changes, no need to change anything at
+    // @JsonIgnore. In contrary, field name should also be updated at
+    // @JsonIgnoreProperties
+    @JsonIgnore
+    private String field2;
+
+    private String field3;
+
+    public SomeBean(String field1, String field2, String field3) {
+        this.field1 = field1;
+        this.field2 = field2;
+        this.field3 = field3;
+    }
+
+    public String getField1() {
+        return field1;
+    }
+
+    public String getField2() {
+        return field2;
+    }
+
+    public String getField3() {
+        return field3;
+    }
+
+    @Override
+    public String toString() {
+        return "SomeBean{" +
+                "field1='" + field1 + '\'' +
+                ", field2='" + field2 + '\'' +
+                ", field3='" + field3 + '\'' +
+                '}';
+    }
+}
