1c394f0bd88cb7ebd48c7c1278952aa94ebb2b06
Abc Def Mac (SourceTree) <69718370+javasamplecodes@users.noreply.github.com>
2023-04-04 12:47:34 +0300

    Update LearningComponent.jsx

--- 13-full-stack/01-frontend/todo-app-myimp/src/components/learning-examples/LearningComponent.jsx
+++ 13-full-stack/01-frontend/todo-app-myimp/src/components/learning-examples/LearningComponent.jsx
@@ -3,6 +3,7 @@ import FirstComponent, {FifthComponent} from "./FirstComponent";
 import SecondComponent from "./SecondComponent";
 import ThirdComponent from "./ThirdComponent";
 import FourthComponent from "./FourthComponent";
+import LearningJavaScript from "./LearningJavaScript";
 
 function LearningComponent() {
     return (
@@ -12,6 +13,7 @@ function LearningComponent() {
             <ThirdComponent />
             <FourthComponent />
             <FifthComponent />
+            <LearningJavaScript />
         </div>
     );
 }
