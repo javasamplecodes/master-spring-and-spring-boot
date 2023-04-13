1a640ea641e7615509dd3cd47c7ccb48ed5a102e
Abc Def Mac (SourceTree) <69718370+javasamplecodes@users.noreply.github.com>
2023-04-13 12:00:06 +0300

    sect11_203_3_improving_login_component_further

--- 13-full-stack/01-frontend/todo-app-myimp/src/components/todo/TodoApp.jsx
+++ 13-full-stack/01-frontend/todo-app-myimp/src/components/todo/TodoApp.jsx
@@ -1,7 +1,9 @@
+import './TodoApp.css'
+import {useState} from "react";
+
 export default function TodoApp() {
     return (
         <div className="TodoApp">
-            Todo Management Application
             <LoginComponent />
             {/*<WelcomeComponent />*/}
         </div>
@@ -9,16 +11,29 @@ export default function TodoApp() {
 }
 
 function LoginComponent() {
+    const [username, setUsername] = useState('omer');
+    const [password, setPassword] = useState('');
+
+    function handleUsernameChange(event) {
+        // console.log(event)
+        // console.log(event.target.value)
+        setUsername(event.target.value)
+    }
+
+    function handlePasswordChange(event) {
+        setPassword(event.target.value)
+    }
+
     return (
         <div className="Login">
             <div className="LoginForm">
                 <div>
                     <label>User Name</label>
-                    <input type="text" name="username" />
+                    <input type="text" name="username" value={username} onChange={handleUsernameChange} />
                 </div>
                 <div>
                     <label>Password</label>
-                    <input type="password" name="password" />
+                    <input type="password" name="password" value={password} onChange={handlePasswordChange} />
                 </div>
                 <div>
                     <button type="button" name="login">login</button>
