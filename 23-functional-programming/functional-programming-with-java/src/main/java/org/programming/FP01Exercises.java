b432e6767e0408872383c35835856d9237a8460a
Omer M <69718370+javasamplecodes@users.noreply.github.com>
2023-04-22 13:32:34 +0300

    sect25_380_6_using_map_in_functional_programs_with_exercises

--- 23-functional-programming/functional-programming-with-java/src/main/java/org/programming/FP01Exercises.java
+++ 23-functional-programming/functional-programming-with-java/src/main/java/org/programming/FP01Exercises.java
@@ -5,8 +5,9 @@ import java.util.List;
 public class FP01Exercises {
 
     public static void main(String[] args) {
+        List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+
         // Exercise#1
-//        List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
 //        printOddNumbersInListFunctional(numbers);
 
         // Exercise#2
@@ -23,18 +24,29 @@ public class FP01Exercises {
 //                .forEach(System.out::println);
 
         // Exercise#4
-        courses.stream()
-                .filter(course -> course.length() >= 4)
-                .forEach(System.out::println);
+//        courses.stream()
+//                .filter(course -> course.length() >= 4)
+//                .forEach(System.out::println);
 
+        // Exercise#5
+//        printCubesOfOddNumbersInListFunctional(numbers);
+
+        // Exercise#6
+//        courses.stream()
+//                .map(course -> course.length())
+//                .forEach(System.out::println);
     }
 
     private static void printOddNumbersInListFunctional(List<Integer> numbers) {
         numbers.stream()
                 .filter(number -> number % 2 != 0) // Lambda Expression
                 .forEach(System.out::println); // Method Reference
-
-        //      .filter(FP01Functional::isEven) // Filter - Only Allow Even Numbers
     }
 
+    private static void printCubesOfOddNumbersInListFunctional(List<Integer> numbers) {
+        numbers.stream()
+                .filter(number -> number % 2 != 0) // Lambda Expression
+                .map(number -> number * number * number)
+                .forEach(System.out::println); // Method Reference
+    }
 }
