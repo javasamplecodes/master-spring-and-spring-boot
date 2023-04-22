49be313cc492e77007508f51b23cc635d0206be5
Omer M <69718370+javasamplecodes@users.noreply.github.com>
2023-04-22 11:57:57 +0300

    sect25_377_3_improving_java_functional_program_with_filter

--- 23-functional-programming/functional-programming-with-java/src/main/java/org/programming/FP01Functional.java
+++ 23-functional-programming/functional-programming-with-java/src/main/java/org/programming/FP01Functional.java
@@ -5,21 +5,29 @@ import java.util.List;
 public class FP01Functional {
 
     public static void main(String[] args) {
-        printAllNumbersInListFunctional(List.of(12, 9, 13, 4, 6, 2, 4, 12, 15));
+        List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+//        printAllNumbersInListFunctional(numbers);
+        printEvenNumbersInListFunctional(numbers);
+
     }
 
-    private static void print(int number) {
-        System.out.println(number);
+//    private static void print(int number) {
+//        System.out.println(number);
+//    }
+
+    private static boolean isEven(int number) {
+        return number % 2 == 0;
     }
 
     private static void printAllNumbersInListFunctional(List<Integer> numbers) {
         numbers.stream()
-                .forEach(FP01Functional::print);
+                .forEach(System.out::println); // Method Reference
+    }
 
-        // How to loop numbers -> structured way
-//        for(int number : numbers) {
-//            System.out.println(number);
-//        }
+    private static void printEvenNumbersInListFunctional(List<Integer> numbers) {
+        numbers.stream()
+                .filter(FP01Functional::isEven) // Filter - Only Allow Even Numbers
+                .forEach(System.out::println); // Method Reference
     }
 
 }
