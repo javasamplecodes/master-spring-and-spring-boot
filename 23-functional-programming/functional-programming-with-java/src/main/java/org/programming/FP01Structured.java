1f0ab13c049a14f68883b36bcc2dc91e6b6a70ee
Omer M <69718370+javasamplecodes@users.noreply.github.com>
2023-04-22 11:29:03 +0300

    sect25_376_2_writing_your_first_java_functional_program

--- 23-functional-programming/functional-programming-with-java/src/main/java/org/programming/FP01Structured.java
+++ 23-functional-programming/functional-programming-with-java/src/main/java/org/programming/FP01Structured.java
@@ -5,13 +5,21 @@ import java.util.List;
 public class FP01Structured {
 
     public static void main(String[] args) {
-        printAllNumbersInListStructured(List.of(12, 9, 13, 4, 6, 2, 4, 12, 15));
+        printAllNumbersInListFunctional(List.of(12, 9, 13, 4, 6, 2, 4, 12, 15));
     }
 
-    private static void printAllNumbersInListStructured(List<Integer> numbers) {
-        for(int number : numbers) {
-            System.out.println(number);
-        }
+    private static void print(int number) {
+        System.out.println(number);
+    }
+
+    private static void printAllNumbersInListFunctional(List<Integer> numbers) {
+        numbers.stream()
+                .forEach(FP01Structured::print);
+
+        // How to loop numbers -> structured way
+//        for(int number : numbers) {
+//            System.out.println(number);
+//        }
     }
 
 }
