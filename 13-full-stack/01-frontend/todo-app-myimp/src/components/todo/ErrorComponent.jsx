6395cc66c1ede89eaabed12a96d01224d7b6cfbd
Omer M <69718370+javasamplecodes@users.noreply.github.com>
2023-05-12 11:39:51 +0300

    sect11_215_14_refactoring_react_components_to_individual_javascript_modules

---
+++ 13-full-stack/01-frontend/todo-app-myimp/src/components/todo/ErrorComponent.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ErrorComponent() {
+    return (
+        <div className="ErrorComponent">
+            <h1>We are working on the problem.</h1>
+            <div>
+                Apologies for the 404. Reach out to our team at ABC.
+            </div>
+        </div>
+    )
+}
+
+export default  ErrorComponent
