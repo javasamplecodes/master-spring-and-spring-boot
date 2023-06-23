3b22c86b4b277e5b38fb3f31f75abe94cf26b95b
Omer M <69718370+javasamplecodes@users.noreply.github.com>
2023-06-24 00:36:46 +0300

    sect16_286_exploring_spring_security_storing_user_credentials_in_memory

--- 71-spring-security/learn-spring-security/src/main/java/com/in28minutes/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ 71-spring-security/learn-spring-security/src/main/java/com/in28minutes/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -5,10 +5,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class BasicAuthSecurityConfiguration {
+
+    enum Role {
+        ADMIN,
+        USER
+    }
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -26,6 +36,21 @@ public class BasicAuthSecurityConfiguration {
         http.csrf().disable();
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("omer")
+                .password("{noop}123")
+                .roles(Role.USER.name())
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}123")
+                .roles(Role.ADMIN.name())
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
 
