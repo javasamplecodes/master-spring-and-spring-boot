8400e809edd7fcc4d1eeae94a6fb1e2ed9d02101
Omer M <69718370+javasamplecodes@users.noreply.github.com>
2023-07-29 16:11:29 +0300

    sect16_296_exploring_spring_security_authorization

--- 71-spring-security/learn-spring-security/src/main/java/com/in28minutes/learnspringsecurity/resources/TodoResource.java
+++ 71-spring-security/learn-spring-security/src/main/java/com/in28minutes/learnspringsecurity/resources/TodoResource.java
@@ -1,12 +1,18 @@
 package com.in28minutes.learnspringsecurity.resources;
 
+import jakarta.annotation.security.RolesAllowed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@EnableMethodSecurity(jsr250Enabled = true, securedEnabled = true)
 public class TodoResource {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
@@ -21,6 +27,10 @@ public class TodoResource {
     }
 
     @GetMapping("/users/{username}/todos")
+    @PreAuthorize("hasRole('USER') and #username == authentication.name")
+    @PostAuthorize("returnObject.username == 'omer'")
+    @RolesAllowed({"ADMIN", "USER"})
+    @Secured({"ROLE_ADMIN", "ROLE_USER"})
     public Todo retrieveTodosForSpecificUser(@PathVariable String username) {
         return TODOS_LIST.get(0);
     }
